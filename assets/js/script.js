570a9bcba1e9b80b263fc1fbcbfbab01baa45310
Bemourn <70115233+koenib@users.noreply.github.com>
2021-11-06 19:05:57 -0600

    add matrix text and first project

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var c = document.getElementById("matrix");
+var ctx = c.getContext("2d");
+fadeColor = "rgba(0, 0, 0, 0.2)";
+ctx.scale(-1, 1);
+
+ctx.fillStyle = "#050505";
+ctx.fillRect(0, 0, c.width, c.height);
+
+//making the canvas set to max size of spotlight image
+c.height = 600;
+c.width = 800;
+
+var font_size = 24;
+var columns = c.width / font_size; //number of columns for the rain
+
+var allowedChars = ` 日ｦｱｳｴｵｶｷｹｺｻｼｽｾｿﾀﾂﾃﾅﾆﾇﾈﾊﾋﾎﾏﾐﾑﾒﾓﾔﾕﾗﾘﾜ٢٣٤٥٦٨٩1234567890ABCDEF:.=*+-¦|_`;
+allowedChars = allowedChars.split("");
+
+var drops = [];
+for (var x = 0; x < columns; x++)
+    drops[x] = Math.floor(Math.random()*(-50)); //start at random y value
+
+function draw() {
+    ctx.fillStyle = fadeColor;
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "rgba(0, 250, 0, 1)";
+    ctx.font = font_size + "px arial";
+
+    for (var i = 0; i < drops.length; i++) {
+        var text = allowedChars[Math.floor(Math.random() * allowedChars.length)];
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+        if (drops[i] * font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        drops[i]++;
+    }
+
+}
+
+setInterval(draw, 40);
